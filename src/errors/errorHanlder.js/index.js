046596deba119091c9b52ec8036986e4c0daf05d
Danielle Luz <daniln2209@gmail.com>
2023-11-30 21:40:11 -0300

    feat: create error handler function

---
+++ src/errors/errorHanlder.js/index.js
@@ -0,0 +1,15 @@
+import { ZodError } from "zod";
+
+export default function errorHandler(error, request, response, next) {
+  let statusCode = 500;
+  let errorMessage = { message: error.errorMessage };
+
+  if (error instanceof AppError) {
+    statusCode = error.statusCode;
+  } else if (error instanceof ZodError) {
+    statusCode = 400;
+    errorMessage = error.flatten().fieldErrors;
+  }
+
+  return response.status(statusCode).json(errorMessage);
+}
