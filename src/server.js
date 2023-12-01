668ca06fb38a8e09f01d2033480f12529cf32b44
Danielle Luz <daniln2209@gmail.com>
2023-11-30 21:35:11 -0300

    feat: create server file

--- src/server.js
+++ src/server.js
@@ -1,26 +1,10 @@
-import "dotenv/config";
-import "reflect-metadata";
-import path from "path";
-import { DataSource } from "typeorm";
+import app from "./app";
+import AppDatasource from "./data-source";
 
-const datasourceConfigurations = () => {
-  const entitiesPath = path.join(__dirname, "/entities/**/*.js");
-  const migrationsPath = path.join(__dirname, "/migrations/**/*.js");
+AppDatasource.initialize().then(() => {
+  console.log("> Database started");
 
-  return {
-    type: process.env.DB_TYPE,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME,
-    synchronize: false,
-    logging: true,
-    entities: [entitiesPath],
-    migrations: [migrationsPath],
-  };
-};
-
-const AppDatasource = new DataSource(datasourceConfigurations);
-
-export default AppDatasource;
+  app.listen(3000, () => {
+    console.log("> Api started");
+  });
+});
