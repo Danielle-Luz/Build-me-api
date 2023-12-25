7a7f4be8c33c63df095f4014e057f7dfdf1916fd
Danielle Luz <daniln2209@gmail.com>
2023-12-25 03:56:58 -0300

    feat: add no permission error default message

--- src/errors/NoPermissionError/index.js
+++ src/errors/NoPermissionError/index.js
@@ -2,11 +2,10 @@ const { StatusCodes } = require("http-status-codes");
 const { AppError } = require("../AppError");
 
 class NoPermissionError extends AppError {
-  constructor(message) {
-    super(
-      "The user is unauthorized to access the route or to perform this operation with other users",
-      StatusCodes.UNAUTHORIZED
-    );
+  constructor(
+    message = "The user is unauthorized to access the route or to perform this operation with other users"
+  ) {
+    super(message, StatusCodes.UNAUTHORIZED);
   }
 }
 
