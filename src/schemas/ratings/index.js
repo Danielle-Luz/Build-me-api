b834e84ee9e20f48620158b0276630f7f766af51
Danielle Luz <daniln2209@gmail.com>
2023-12-25 03:58:21 -0300

    feat: remove author property from schema

--- src/schemas/ratings/index.js
+++ src/schemas/ratings/index.js
@@ -3,14 +3,12 @@ const z = require("zod");
 const newRatingSchema = z.object({
   comment: z.string(),
   grade: z.number().int().min(0).max(5),
-  authorId: z.number().int(),
   ratedRecipientId: z.number().int(),
 });
 
 const updatedRatingSchema = newRatingSchema
-  .pick({
-    comment: true,
-    grade: true,
+  .omit({
+    ratedRecipientId: true,
   })
   .partial();
 
