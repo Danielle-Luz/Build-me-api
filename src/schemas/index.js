ec1079cc3f8b766d9ffd0ed31926770f88195a9b
Danielle Luz <daniln2209@gmail.com>
2023-12-25 02:21:33 -0300

    feat: add ratings schemas

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -8,12 +8,15 @@ const {
   newPermissionSchema,
   updatedPermissionSchema,
 } = require("./permissions/index");
+const { newRatingSchema, updatedRatingSchema } = require("./ratings");
 
 module.exports = {
   nameSchema,
   newPermissionSchema,
+  newRatingSchema,
   newUserSchema,
   userLoginSchema,
   updatedPermissionSchema,
+  updatedRatingSchema,
   updatedUserSchema,
 };
