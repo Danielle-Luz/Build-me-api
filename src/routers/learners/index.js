85b9c21b5caf23e98c756280254da7dcc593911f
Danielle Luz <daniln2209@gmail.com>
2023-12-26 18:20:11 -0300

    feat: add middlewares on learners router

--- src/routers/learners/index.js
+++ src/routers/learners/index.js
@@ -1,15 +1,34 @@
 const { Router } = require("express");
 const { LearnersController } = require("../../controllers");
+const { UtilsMiddlewares, UsersMiddlewares } = require("../../middlewares");
+const { newLearnerSchema, updatedLearnerSchema } = require("../../schemas");
 
 const learnersRouter = Router();
 
-learnersRouter.post("/", LearnersController.create);
+learnersRouter.post(
+  "/",
+  UtilsMiddlewares.validateSchema(newLearnerSchema),
+  UsersMiddlewares.isTokenFilled,
+  UsersMiddlewares.validateToken,
+  LearnersController.create
+);
 
 learnersRouter.get("/", LearnersController.getAll);
 learnersRouter.get("/:id", LearnersController.getById);
 
-learnersRouter.patch("/:id", LearnersController.update);
+learnersRouter.patch(
+  "/:id",
+  UsersMiddlewares.isTokenFilled,
+  UsersMiddlewares.validateToken,
+  UtilsMiddlewares.validateSchema(updatedLearnerSchema),
+  LearnersController.update
+);
 
-learnersRouter.delete("/:id", LearnersController.delete);
+learnersRouter.delete(
+  "/:id",
+  UsersMiddlewares.isTokenFilled,
+  UsersMiddlewares.validateToken,
+  LearnersController.delete
+);
 
 module.exports = { learnersRouter };
