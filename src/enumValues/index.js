36af17329e6690de671cc7edf54a10f047bbdc4a
Danielle Luz <daniln2209@gmail.com>
2023-12-28 17:39:09 -0300

    feat: create skill levels array and change export method

--- src/enumValues/index.js
+++ src/enumValues/index.js
@@ -1,5 +1,9 @@
-exports.memberSelectionMethod = [
+const memberSelectionMethod = [
   "Aleatória",
   "Ordem de inscrição",
   "Escolha manual",
 ];
+
+const skillLevels = ["Básico", "Intermediário", "Avançado"];
+
+module.exports = [memberSelectionMethod, skillLevels];
