e01ce5e4febc8afddaacb26be9f99fccfd4af3ef
Danielle Luz <daniln2209@gmail.com>
2023-12-31 22:54:36 -0300

    feat: allow null as chosen candidate

--- src/schemas/vacancies/index.js
+++ src/schemas/vacancies/index.js
@@ -5,7 +5,7 @@ const newVacancySchema = z.object({
   description: z.string().optional(),
   learnersLimit: z.number().int(),
   projectId: z.number().int(),
-  chosenCandidateId: z.number().int().optional(),
+  chosenCandidateId: z.union([z.number().int().optional(), z.null()]),
 });
 
 const updatedVacancySchema = newVacancySchema
