bbb352407a169b8ef6b9b551986b3e960ee62070
Danielle Luz <daniln2209@gmail.com>
2024-01-04 17:28:45 -0300

    feat: add rankings' routes

--- src/routers/rankings/index.js
+++ src/routers/rankings/index.js
@@ -4,8 +4,33 @@ const { RankingsController } = require("../../controllers");
 const rankingsRouter = Router();
 
 rankingsRouter.get(
-  "/moreParticipations",
+  "/vacancies/participations",
   RankingsController.getUsersWithMoreVacanciesParticipations
+);
+
+rankingsRouter.get(
+  "/ratings/authored",
+  RankingsController.getUsersWithMoreRatingsMade
+);
+
+rankingsRouter.get(
+  "/ratings/average",
+  RankingsController.getUsersWithBiggestAverageRatings
+);
+
+rankingsRouter.get(
+  "/learners/subscriptions",
+  RankingsController.getUsersWithMoreLearnerSubscriptions
+);
+
+rankingsRouter.get(
+  "/tests/scores",
+  RankingsController.getUsersWithBiggestTestScores
+);
+
+rankingsRouter.get(
+  "/userSkills/scores",
+  RankingsController.getUsersWithBestSkillScores
 );
 
 module.exports = { rankingsRouter };
