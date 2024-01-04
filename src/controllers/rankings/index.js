89605220d736893572cb486dd83ee00b2ee073d2
Danielle Luz <daniln2209@gmail.com>
2024-01-04 17:28:31 -0300

    feat: create rankings' controller

--- src/controllers/rankings/index.js
+++ src/controllers/rankings/index.js
@@ -5,10 +5,37 @@ const { RankingsService } = require("../../services");
 
 class RankingsController {
   static async getUsersWithMoreVacanciesParticipations(request, response) {
-    const mostVacanciesApplications =
+    const orderedResults =
       await RankingsService.getUsersWithMoreVacanciesParticipations();
+    return response.status(StatusCodes.OK).json(orderedResults);
+  }
 
-    return response.status(StatusCodes.OK).json(mostVacanciesApplications);
+  static async getUsersWithMoreRatingsMade(request, response) {
+    const orderedResults = await RankingsService.getUsersWithMoreRatingsMade();
+    return response.status(StatusCodes.OK).json(orderedResults);
+  }
+
+  static async getUsersWithBiggestAverageRatings(request, response) {
+    const orderedResults =
+      await RankingsService.getUsersWithBiggestAverageRatings();
+    return response.status(StatusCodes.OK).json(orderedResults);
+  }
+
+  static async getUsersWithMoreLearnerSubscriptions(request, response) {
+    const orderedResults =
+      await RankingsService.getUsersWithMoreLearnerSubscriptions();
+    return response.status(StatusCodes.OK).json(orderedResults);
+  }
+
+  static async getUsersWithBiggestTestScores(request, response) {
+    const orderedResults =
+      await RankingsService.getUsersWithBiggestTestScores();
+    return response.status(StatusCodes.OK).json(orderedResults);
+  }
+
+  static async getUsersWithBestSkillScores(request, response) {
+    const orderedResults = await RankingsService.getUsersWithBestSkillScores();
+    return response.status(StatusCodes.OK).json(orderedResults);
   }
 }
 
