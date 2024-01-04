0563dbca634611bd07630c7dd2925cc0d1885f25
Danielle Luz <daniln2209@gmail.com>
2024-01-04 17:28:01 -0300

    feat: add rankings' service

--- src/services/rankings/index.js
+++ src/services/rankings/index.js
@@ -1,5 +1,11 @@
 const { AppDatasource } = require("../../data-source");
-const { Vacancies } = require("../../entities");
+const {
+  Vacancies,
+  Ratings,
+  Learners,
+  Tests,
+  UserSkills,
+} = require("../../entities");
 
 class RankingsService {
   static async getUsersWithMoreVacanciesParticipations() {
@@ -7,18 +13,136 @@ class RankingsService {
       .select("COUNT(vacancies.chosenCandidateId)", "applicationsCount")
       .from(Vacancies, "vacancies")
       .innerJoin("vacancies.chosenCandidate", "candidate")
+      .addSelect(`candidate."id"`, "userId")
       .addSelect([
-        `candidate."id"`,
         `candidate."firstName"`,
         `candidate."lastName"`,
         `candidate."profilePicture"`,
       ])
-      .groupBy(`vacancies."chosenCandidateId"`)
-      .addGroupBy("candidate.id")
-      .addGroupBy(`candidate."firstName"`)
-      .addGroupBy(`candidate."lastName"`)
-      .addGroupBy(`candidate."profilePicture"`)
+      .groupBy([
+        `vacancies."chosenCandidateId"`,
+        "candidate.id",
+        `candidate."firstName"`,
+        `candidate."lastName"`,
+        `candidate."profilePicture"`,
+      ])
       .orderBy(`"applicationsCount"`, "DESC")
+      .getRawMany();
+  }
+
+  static async getUsersWithMoreRatingsMade() {
+    return AppDatasource.createQueryBuilder()
+      .select("COUNT(*)", "ratingsMadeCount")
+      .from(Ratings, "ratings")
+      .innerJoin("ratings.author", "author")
+      .addSelect(`author."id"`, "userId")
+      .addSelect([
+        `author."id"`,
+        `author."firstName"`,
+        `author."lastName"`,
+        `author."profilePicture"`,
+      ])
+      .groupBy([
+        `ratings."authorId"`,
+        "author.id",
+        `author."firstName"`,
+        `author."lastName"`,
+        `author."profilePicture"`,
+      ])
+      .orderBy(`"ratingsMadeCount"`, "DESC")
+      .getRawMany();
+  }
+
+  static async getUsersWithBiggestAverageRatings() {
+    return AppDatasource.createQueryBuilder()
+      .select("ROUND(AVG(ratings.grade), 2)", "averageRating")
+      .from(Ratings, "ratings")
+      .innerJoin("ratings.ratedRecipient", "recipient")
+      .addSelect(`recipient."id"`, "userId")
+      .addSelect([
+        `recipient."firstName"`,
+        `recipient."lastName"`,
+        `recipient."profilePicture"`,
+      ])
+      .groupBy([
+        `ratings."ratedRecipientId"`,
+        "recipient.id",
+        `recipient."firstName"`,
+        `recipient."lastName"`,
+        `recipient."profilePicture"`,
+      ])
+      .orderBy(`"averageRating"`, "DESC")
+      .getRawMany();
+  }
+
+  static async getUsersWithMoreLearnerSubscriptions() {
+    return AppDatasource.createQueryBuilder()
+      .select("COUNT(*)", "learnerSubscriptionsCount")
+      .from(Learners, "learners")
+      .innerJoin("learners.candidate", "candidate")
+      .addSelect(`candidate."id"`, "userId")
+      .addSelect([
+        `candidate."firstName"`,
+        `candidate."lastName"`,
+        `candidate."profilePicture"`,
+      ])
+      .groupBy([
+        `learners."candidateId"`,
+        "candidate.id",
+        `candidate."firstName"`,
+        `candidate."lastName"`,
+        `candidate."profilePicture"`,
+      ])
+      .orderBy(`"learnerSubscriptionsCount"`, "DESC")
+      .getRawMany();
+  }
+
+  static async getUsersWithBiggestTestScores() {
+    return AppDatasource.createQueryBuilder()
+      .select("SUM(tests.score)", "totalTestsScore")
+      .from(Tests, "tests")
+      .innerJoin("tests.user", "applicant")
+      .addSelect(`applicant."id"`, "userId")
+      .addSelect([
+        `applicant."firstName"`,
+        `applicant."lastName"`,
+        `applicant."profilePicture"`,
+      ])
+      .groupBy([
+        `tests."userId"`,
+        "applicant.id",
+        `applicant."firstName"`,
+        `applicant."lastName"`,
+        `applicant."profilePicture"`,
+      ])
+      .orderBy(`"totalTestsScore"`, "DESC")
+      .getRawMany();
+  }
+
+  static async getUsersWithBestSkillScores() {
+    return AppDatasource.createQueryBuilder()
+      .select(`candidate."id"`, "userId")
+      .addSelect([
+        `candidate."firstName"`,
+        `candidate."lastName"`,
+        `candidate."profilePicture"`,
+      ])
+      .addSelect(`technology."name"`, "technologyName")
+      .addSelect("skills.score", "skillScore")
+      .from(UserSkills, "skills")
+      .innerJoin("skills.user", "candidate")
+      .innerJoin("skills.technology", "technology")
+      .addSelect()
+      .groupBy([
+        `technology."name"`,
+        `skills."userId"`,
+        "skills.score",
+        "candidate.id",
+        `candidate."firstName"`,
+        `candidate."lastName"`,
+        `candidate."profilePicture"`,
+      ])
+      .orderBy("skills.score", "DESC")
       .getRawMany();
   }
 }
