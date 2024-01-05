55cafcd5d7cdaf7f5411fa78da16ff68221610d4
Danielle Luz <daniln2209@gmail.com>
2024-01-05 13:20:02 -0300

    feat: create votes' controller

--- src/controllers/votes/index.js
+++ src/controllers/votes/index.js
@@ -4,10 +4,29 @@ const { VotesService } = require("../../services");
 class VotesController {
   static async create(request, response) {
     const { validatedData } = request;
+    const { projectId } = request.foundVacancy;
 
-    const createdVote = await VotesService.create(validatedData);
+    validatedData.voterId = request.loggedUser.id;
+
+    const createdVote = await VotesService.create(validatedData, projectId);
 
     return response.status(StatusCodes.CREATED).json(createdVote);
+  }
+
+  static async getUserVote(request, response) {
+    const { votationId } = request.params;
+    const voterId = request.loggedUser.id;
+    const foundVote = await VotesService.getUserVote(voterId, votationId);
+
+    return response.status(StatusCodes.OK).json(foundVote);
+  }
+
+  static async delete(request, response) {
+    const { voteId } = request.params;
+
+    await VotesService.delete(voteId);
+
+    return response.status(StatusCodes.NO_CONTENT).json();
   }
 }
 
