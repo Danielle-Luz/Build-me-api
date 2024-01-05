e06f78a46eefae2e522688a4cf800b4b8e88fc69
Danielle Luz <daniln2209@gmail.com>
2024-01-05 13:21:01 -0300

    feat: modify votes and votations routes

--- src/routers/votations/index.js
+++ src/routers/votations/index.js
@@ -1,29 +1,36 @@
 const { Router } = require("express");
 const { VotationsController } = require("../../controllers/votations");
 const { VotationsMiddlewares } = require("../../middlewares/votations");
+const { UtilsMiddlewares, UsersMiddlewares } = require("../../middlewares");
+const { newVotationSchema } = require("../../schemas");
 
 const votationsRouter = Router();
 
 votationsRouter.post(
   "/",
+  UtilsMiddlewares.validateSchema(newVotationSchema),
+  UsersMiddlewares.isTokenFilled,
+  UsersMiddlewares.validateToken,
+  VotationsMiddlewares.isVotationCreatorEqualToProjectOwner,
   VotationsMiddlewares.doesVacancyExist,
   VotationsController.create
 );
 
 votationsRouter.get(
   "/projects/:projectId",
+  UsersMiddlewares.isTokenFilled,
+  UsersMiddlewares.validateToken,
+  VotationsMiddlewares.doesProjectExist,
+  VotationsMiddlewares.isVacancyProjectMember,
   VotationsController.getVotationsByProject
-);
-
-votationsRouter.patch(
-  "/:votationId",
-  VotationsMiddlewares.doesVacancyExist,
-  VotationsController.update
 );
 
 votationsRouter.delete(
   "/:votationId",
+  UsersMiddlewares.isTokenFilled,
+  UsersMiddlewares.validateToken,
   VotationsMiddlewares.doesVacancyExist,
+  VotationsMiddlewares.isVotationCreatorEqualToProjectOwner,
   VotationsController.delete
 );
 
--- src/routers/votes/index.js
+++ src/routers/votes/index.js
@@ -1,8 +1,39 @@
 const { Router } = require("express");
 const { VotesController } = require("../../controllers");
+const {
+  UtilsMiddlewares,
+  UsersMiddlewares,
+  VotesMiddlewares,
+} = require("../../middlewares");
+const { newVoteSchema } = require("../../schemas");
 
 const votesRouter = Router();
 
-votesRouter.post("/", VotesController.create);
+votesRouter.post(
+  "/",
+  UtilsMiddlewares.validateSchema(newVoteSchema),
+  UsersMiddlewares.isTokenFilled,
+  UsersMiddlewares.validateToken,
+  VotesMiddlewares.doesVotationExist,
+  VotesMiddlewares.isVotingInProjectColleague,
+  VotesMiddlewares.didUserVotedOnce,
+  VotesController.create
+);
+
+votesRouter.get(
+  "/votations/:votationId",
+  UsersMiddlewares.isTokenFilled,
+  UsersMiddlewares.validateToken,
+  VotesMiddlewares.doesVotationExist,
+  VotesController.getUserVote
+);
+
+votesRouter.delete(
+  "/:voteId",
+  UsersMiddlewares.isTokenFilled,
+  UsersMiddlewares.validateToken,
+  VotesMiddlewares.isVoteFromLoggedUser,
+  VotesController.delete
+);
 
 module.exports = { votesRouter };
