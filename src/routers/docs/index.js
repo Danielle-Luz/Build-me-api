15982c024d83bcf99eb8b5600a3a251bdffe9aa2
Danielle Luz <daniln2209@gmail.com>
2024-01-28 04:05:59 -0300

    feat: add swagger configuration

--- src/routers/docs/index.js
+++ src/routers/docs/index.js
@@ -4,7 +4,13 @@ const swaggerDocument = require("./../../docs/swagger.json");
 
 const docsRouter = Router();
 
-docsRouter.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+docsRouter.use(
+  "/",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: { supportedSubmitMethods: ["get"] },
+  })
+);
 docsRouter.get("/docs.json", (request, response) => {
   response.setHeader("Content-Type", "application/json");
   return response.send(swaggerDocument);
